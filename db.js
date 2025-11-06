46c3f7fa6b9472fbbe4ba242dc941f4bb2a14dba
LataSingh07 <latakumarisingh5574@gmail.com>
2025-11-06 16:03:58 +0530

    Db connected

---
+++ db.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const { Schema, Types } = mongoose;
+
+console.log("Connecting to MongoDB...");
+
+
+mongoose.connect(
+  "mongodb+srv://lataSingh:kqstfFLEUPcETkpD@cluster0.adatax7.mongodb.net/course_selling_website")
+.then(() => console.log(" Connected to MongoDB"))
+.catch((err) => console.error(" MongoDB connection error:", err));
+
+const userSchema = new Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+});
+
+const adminSchema = new Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+});
+
+const courseSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  imageLink: { type: String },
+  createdBy: { type: Types.ObjectId, ref: "Admin", required: true },
+});
+
+const purchaseSchema = new Schema({
+  userId: { type: Types.ObjectId, ref: "User", required: true },
+  courseId: { type: Types.ObjectId, ref: "Course", required: true },
+});
+
+const UserModel = mongoose.model("User", userSchema);
+const AdminModel = mongoose.model("Admin", adminSchema);
+const CourseModel = mongoose.model("Course", courseSchema);
+const PurchaseModel = mongoose.model("Purchase", purchaseSchema);
+
+
+module.exports = {
+  UserModel,
+  AdminModel,
+  CourseModel,
+  PurchaseModel,
+};
